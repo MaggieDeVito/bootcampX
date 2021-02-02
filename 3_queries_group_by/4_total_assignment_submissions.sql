63e14bd2e0ef898f406d253e44c1818c5157b674
MaggieDeVito <maggiemaydevito@gmail.com>
2021-02-02 03:48:53 +0000

    created more queries using Group By and Having

---
+++ 3_queries_group_by/4_total_assignment_submissions.sql
@@ -0,0 +1,4 @@
+SELECT cohorts.name AS cohort, count(assignment_submissions.id) AS total_submissions
+FROM cohorts JOIN students ON cohorts.id = cohort_id JOIN assignment_submissions ON students.id = student_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
